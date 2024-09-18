0b06d67da43ced4b4a395d3b98de084591e57f1d
simonegaffurini <simone.gaffurini@regestaitalia.it>
2024-09-18 20:44:15 +0200

    set sync/async

--- src/userExit/UserExit.ts
+++ src/userExit/UserExit.ts
@@ -1,3 +1,4 @@
+import { Logger } from "../logger";
 import { IUserExit } from "./IUserExit";
 import { listExitModules } from "./listExitModules";
 
@@ -13,40 +14,57 @@ export class UserExitManager {
     public static async createInstance(): Promise<void> {
         const aExitModules = [];
         const exitModules = await listExitModules();
-        for(const exitModule of exitModules){
-            try{
+        for (const exitModule of exitModules) {
+            try {
                 const exitImport = await import(exitModule);
                 aExitModules.push({
                     module: exitModule,
                     instance: new exitImport.MyExit()
                 });
-            }catch(e){
-                debugger
+            } catch (e) {
+                Logger.warning(`Exit module ${exitModule} couldn't be imported: ${e.toString()}`);
             }
         }
         UserExitManager.instance = new UserExitManager(aExitModules);
     }
 
-    public static get(): IUserExit {
+    public static get(sync: boolean = false): IUserExit {
         return new Proxy({} as IUserExit, {
             get: (_, methodName: string | symbol) => {
                 return (...args: any[]) => {
                     var exitResults = [];
-                    for(const userExit of UserExitManager.instance._modules){
-                        const method = (userExit.instance as any)[methodName];
-                        if (typeof method === 'function') {
-                            userExit.instance.prevUserExit = exitResults;
-                            var result = method.apply(userExit.instance, args);
-                            exitResults.push({
-                                module: userExit.module,
-                                result: result
-                            });
-                        }else{
-                            //method not implemented in exit class
+                    const getResult = async () => {
+                        for (const userExit of UserExitManager.instance._modules) {
+                            const method = (userExit.instance as any)[methodName];
+                            if (typeof method === 'function') {
+                                userExit.instance.prevUserExit = exitResults;
+                                var result = method.apply(userExit.instance, args);
+                                if (result instanceof Promise) {
+                                    exitResults.push({
+                                        module: userExit.module,
+                                        result: await result
+                                    });
+                                } else {
+                                    exitResults.push({
+                                        module: userExit.module,
+                                        result: result
+                                    });
+                                }
+                            } else {
+                                //method not implemented in exit class
+                            }
+                        }
+                        if (exitResults.length > 0) {
+                            return exitResults[exitResults.length - 1].result;
                         }
                     }
-                    if(exitResults.length > 0){
-                        return exitResults[exitResults.length -1].result;
+                    if(!sync){
+                        return getResult();
+                    }else{
+                        getResult();
+                        if (exitResults.length > 0) {
+                            return exitResults[exitResults.length - 1].result;
+                        }
                     }
                 };
             }
