24c30ce43eff58a6d74b13a252878d52455858b3
simonegaffurini <simone.gaffurini@regestaitalia.it>
2025-03-05 10:00:15 +0100

    remove get dot abapgit

--- src/actions/publish/getSourceCode.ts
+++ src/actions/publish/getSourceCode.ts
@@ -2,16 +2,13 @@ import { Step } from "@simonegaffurini/sammarksworkflow";
 import { PublishWorkflowContext } from ".";
 import { Logger } from "../../logger";
 import { SystemConnector } from "../../systemConnector";
-import { DotAbapGit } from "../../abapgit";
 
 /**
  * Get source code (if abapgit developer is installed)
  * 
- * 1- read dot abapgit
+ * 1- get abapgit source code and object list
  * 
- * 2- get abapgit source code and object list
- * 
- * 3- get ignored objects
+ * 2- get ignored objects
  * 
 */
 export const getSourceCode: Step<PublishWorkflowContext> = {
@@ -22,15 +19,12 @@ export const getSourceCode: Step<PublishWorkflowContext> = {
         try{
             Logger.loading(`Reading ${context.rawInput.packageData.devclass} source code...`);
 
-            //1- read dot abapgit
-            context.runtime.abapGitData.dotAbapGit = await DotAbapGit.fromDevclass(context.rawInput.packageData.devclass);
-
-            //2- get abapgit source code and object list
+            //1- get abapgit source code and object list
             const sourceCode = await SystemConnector.getAbapgitSource(context.rawInput.packageData.devclass);
             context.runtime.abapGitData.sourceCode.zip = sourceCode.zip;
             context.runtime.abapGitData.sourceCode.objects = sourceCode.objects;
 
-            //3- get ignored objects
+            //2- get ignored objects
             context.runtime.packageData.tadir.forEach(o => {
                 const object = context.runtime.abapGitData.sourceCode.objects.find(k => k.pgmid === o.pgmid && k.object === o.object && k.objName === o.objName);
                 if(!object){
