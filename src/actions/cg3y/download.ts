02bee53c7f2618f9be02115cca11b1b7dd073d1a
Simone Gaffurini <simone.gaffurini@regestaitalia.it>
2026-03-09 18:36:49 +0100

    8.5.0

---
+++ src/actions/cg3y/download.ts
@@ -0,0 +1,42 @@
+import { Step } from "@simonegaffurini/sammarksworkflow";
+import { Cg3yWorkflowContext } from ".";
+import { Transport } from "../../transport";
+import { Logger } from "trm-commons";
+import * as AdmZip from "adm-zip";
+
+/**
+ * Download
+ * 
+ * 1- check is released
+ * 
+ * 2- download
+ * 
+ * 3- zip
+ * 
+*/
+export const download: Step<Cg3yWorkflowContext> = {
+    name: 'download',
+    run: async (context: Cg3yWorkflowContext): Promise<void> => {
+        Logger.log('Download step', true);
+        const transport = new Transport(context.rawInput.trkorr);
+
+        //1- check is released
+        const isReleased = await transport.isReleased();
+        if (!isReleased) {
+            throw new Error(`Transport "${transport.trkorr}" is not released. To download, release it first.`);
+        }
+
+        //2- download
+        Logger.loading(`Downloading transport ${Transport.getTransportIcon()}  ${transport.trkorr}...`);
+        const data = await transport.download();
+
+        //3- zip
+        const zip = new AdmZip.default();
+        zip.addFile(data.filenames.header, data.binaries.header);
+        zip.addFile(data.filenames.data, data.binaries.data);
+        const buffer = await zip.toBufferPromise();
+        context.output = {
+            binaries: buffer
+        }
+    }
+}
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -4,3 +4,5 @@ export * from "./checkPackageDependencies"
 export * from "./checkSapEntries";
 export * from "./install";
 export * from "./publish";
+export * from "./cg3y";
+export * from "./cg3z";
