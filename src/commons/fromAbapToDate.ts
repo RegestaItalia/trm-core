a0bdd9409399cdec1779d593635c6da9bf435238
Simone Gaffurini <simone.gaffurini@regestaitalia.it>
2026-07-29 17:58:55 +0200

    9.10.3

--- src/commons/fromAbapToDate.ts
+++ src/commons/fromAbapToDate.ts
@@ -14,21 +14,24 @@ function fromAbapToDateLegacy(as4date: AS4DATE, as4time: AS4TIME): Date {
     return new Date(year, month, day, hour, minutes, seconds);
 }
 
+function parseDate(as4date: AS4DATE, as4time: AS4TIME, ianaZone: string): DateTime {
+    const abapDateTime = DateTime.fromFormat(
+        `${as4date} ${as4time}`,
+        "yyyyMMdd HHmmss",
+        { zone: ianaZone }
+    );
+
+    if (abapDateTime.isValid) {
+        return abapDateTime;
+    }
+
+    return DateTime.fromISO(`${as4date}T${as4time}`, { zone: ianaZone });
+}
+
 export async function fromAbapToDate(as4date: AS4DATE, as4time: AS4TIME): Promise<Date> {
     const ianaZone = await SystemConnector.getTimezone();
 
-    const year = Number(as4date.slice(0, 4));
-    const month = Number(as4date.slice(4, 6));
-    const day = Number(as4date.slice(6, 8));
-
-    const hour = Number(as4time.slice(0, 2));
-    const minute = Number(as4time.slice(2, 4));
-    const second = Number(as4time.slice(4, 6));
-
-    const dt = DateTime.fromObject(
-        { year, month, day, hour, minute, second },
-        { zone: ianaZone }
-    );
+    const dt = parseDate(as4date, as4time, ianaZone);
 
     if (!dt.isValid) {
         return fromAbapToDateLegacy(as4date, as4time);
